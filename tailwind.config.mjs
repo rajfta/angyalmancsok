b7a8fd963982a3d008d0ba9fb0a5ce55ce2b7789
AdamReinmuller <adam.rajmuller@gmail.com>
2024-10-23 21:48:15 +0200

    switch to shadcn from antd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ["class"],
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {
-            colors: {
-                primary: "#81b3c9",
-                bg: {
-                    primary: "#e2dad2",
-                    header: "#EEECEA",
-                },
-                text: {
-                    primary: "#3b3b3b",
-                },
-            },
-        },
+    	extend: {
+    		colors: {
+    			primary: '#81b3c9',
+    			bg: {
+    				primary: '#e2dad2',
+    				header: '#EEECEA'
+    			},
+    			text: {
+    				primary: '#3b3b3b'
+    			}
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
-    plugins: [],
+    plugins: [require("tailwindcss-animate")],
 };
