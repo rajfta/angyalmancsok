675c6250887a46db8fc45c5488f4bf58cd0fbe89
AdamReinmuller <adam.rajmuller@gmail.com>
2024-10-23 20:28:41 +0200

    header positions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,97 @@
-import { type FC } from "react";
+import { useState, type FC, type ReactNode } from "react";
+import { Button, Drawer } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+
+import logo from "../assets/logo.png";
 
 const Header: FC = () => {
-  return (
-    <header className="bg-primary text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Angyalmancsok</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li>
-              <a href="/" className="hover:text-gray-300">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="/about" className="hover:text-gray-300">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/services" className="hover:text-gray-300">
-                Services
-              </a>
-            </li>
-            <li>
-              <a href="/contact" className="hover:text-gray-300">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
+    return (
+        <header className="bg-bg-header relative h-20 w-full px-4 py-2 flex items-center justify-between">
+            <div className="h-full aspect-square hover:scale-110 transition-all duration-300">
+                <a href="/">
+                    <img src={logo.src} alt="Angyalmancsok logo" />
+                </a>
+            </div>
+            <div className="flex md:flex-row-reverse gap-4 items-center justify-between">
+                <Button
+                    size="large"
+                    className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:static md:transform-none"
+                    type="primary"
+                >
+                    <a href="/adomanyozas">Adományozás</a>
+                </Button>
+                <Navigation />
+            </div>
+        </header>
+    );
 };
+
+const Navigation: FC = () => {
+    return (
+        <>
+            <div className="block md:hidden">
+                <MobileNav />
+            </div>
+
+            <div className="hidden md:block">
+                <NavLinks />
+            </div>
+        </>
+    );
+};
+
+const MobileNav: FC = () => {
+    const [open, setOpen] = useState(false);
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <Button type="text" onClick={showDrawer} icon={<MenuOutlined />} />
+            <Drawer size="default" onClose={onClose} open={open}>
+                <NavLinks />
+            </Drawer>
+        </>
+    );
+};
+
+const NavLinks: FC = () => {
+    return (
+        <nav>
+            <ul className="flex flex-col md:flex-row gap-4 items-center justify-center">
+                <li>
+                    <Link href="/">Főoldal</Link>
+                </li>
+                <li>
+                    <Link href="/rolunk">Rólunk</Link>
+                </li>
+                <li>
+                    <Link href="/szolgaltatasok">Szolgáltatások</Link>
+                </li>
+                <li>
+                    <Link href="/kapcsolat">Kapcsolat</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+const Link: FC<{ href: string; children: ReactNode }> = ({
+    href,
+    children,
+}) => (
+    <a
+        href={href}
+        className="text-text-primary hover:text-primary transition-colors duration-300 text-lg"
+    >
+        {children}
+    </a>
+);
 
 export default Header;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#000000",
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#81b3c9",
+                bg: {
+                    primary: "#e2dad2",
+                    header: "#EEECEA",
+                },
+                text: {
+                    primary: "#3b3b3b",
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
