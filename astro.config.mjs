5749cf8e0fa9919755592742eb234d3d46c1c9ea
rajfta <rajfta@gmail.com>
2025-12-11 10:05:34 +0100

    prerender pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	output: "server",
+	adapter: vercel(),
 	integrations: [mdx(), react()],
 	vite: {
 		plugins: [tailwindcss()],
